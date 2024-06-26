0be633380bc888a6a102524a2de49e7830c00268
Arindam-db <debarindam225@gmail.com>
2024-06-26 21:40:32 +0530

    first

---
+++ app/src/main/java/com/nrh/musicopen/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.nrh.musicopen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 1500 // 1.5 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(splashTimeOut)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
